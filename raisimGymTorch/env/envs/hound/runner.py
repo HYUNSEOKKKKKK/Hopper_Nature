fe47f13f8ffab4ef18d4ae77abed6839d68e4db5
GijeongKim <kijoung0963@kaist.ac.kr>
2023-11-23 19:01:49 +0900

    .

--- raisimGymTorch/env/envs/hound/runner.py
+++ raisimGymTorch/env/envs/hound/runner.py
@@ -136,7 +136,7 @@ for update in range(8000):
         ppo.step(value_obs=value_obs, rews=reward, dones=dones)
         done_sum = done_sum + np.sum(dones)
         reward_sum = reward_sum + np.sum(reward)
-        if (update % 20 == 0) or (update % 200 == 0) or (update % 202 == 0):
+        if (update % 200 == 0) or (update % 202 == 0):
             reward_analyzer.add_reward_info(env.get_reward_info())
 
     # take st step to get value obs
@@ -157,7 +157,7 @@ for update in range(8000):
     end = time.time()
     scheduler.step()
 
-    if (update % 20 == 0) or (update % 200 == 0) or (update % 202 == 0):
+    if (update % 200 == 0) or (update % 202 == 0):
         reward_analyzer.analyze_and_plot(update)
 
     print('----------------------------------------------------')
