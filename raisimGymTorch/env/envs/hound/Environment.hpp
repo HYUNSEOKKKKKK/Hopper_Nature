ce3da64a08797af416db80832262fd0be8616e9f
GijeongKim <kijoung0963@kaist.ac.kr>
2023-11-28 14:51:37 +0900

    .

--- raisimGymTorch/env/envs/hound/Environment.hpp
+++ raisimGymTorch/env/envs/hound/Environment.hpp
@@ -58,8 +58,8 @@ class ENVIRONMENT : public RaisimGymEnv {
     /// action scaling
     actionMean_ = gcInit_.tail(12);
     for (int i=0; i<4; i++){
-//      actionStd_.segment(i*3,3) << 0.1, 0.15, 0.15;
-      actionStd_.segment(i*3,3) << 0.133, 0.2, 0.2;
+      actionStd_.segment(i*3,3) << 0.1, 0.15, 0.15;
+//      actionStd_.segment(i*3,3) << 0.133, 0.2, 0.2;
     }
 
     /// Reward coefficients
