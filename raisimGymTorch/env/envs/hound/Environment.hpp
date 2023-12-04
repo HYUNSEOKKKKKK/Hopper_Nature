6218a0d17451554ed9ebf7b79b9be4cd9a6a4c8d
GijeongKim <kijoung0963@kaist.ac.kr>
2023-12-04 18:16:02 +0900

    35.9

--- raisimGymTorch/env/envs/hound/Environment.hpp
+++ raisimGymTorch/env/envs/hound/Environment.hpp
@@ -283,7 +283,7 @@ class ENVIRONMENT : public RaisimGymEnv {
       } else {
           limitBaseMotion_.row(0) << -0.1,0.1;
           limitBaseMotion_.row(1) << -0.1,0.1;
-          jointVelTemp_ = gv_.tail(12);
+//          jointVelTemp_ = gv_.tail(12);
           standingSmoothness_ = 1.5;
       }
   }
@@ -335,8 +335,8 @@ class ENVIRONMENT : public RaisimGymEnv {
           /// footClearance_ -> limit_foot_clearance 에 있도록 (-0.12,0.12) -> foot 드는 거 enforcing
           double desiredFootZPosition = 0.15;
           for (int i=0; i<4; i++){
-//              if (footContactPhase_(i) < -0.8) { /// during swing
-              if (footContactPhase_(i) < -0.6) { /// during swing
+              if (footContactPhase_(i) < -0.8) { /// during swing
+//              if (footContactPhase_(i) < -0.6) { /// during swing
                   footClearance_(i) =
                           footToTerrain_.segment(i * 5, 5).minCoeff() - desiredFootZPosition; // 대략, 0.17 sec, 0 보다 크거나 같으면 됨 (enforcing clearance)
               }else{ footClearance_(i) = 0.0; } // max reward (not enforcing clearance)
--- raisimGymTorch/env/envs/hound/tester.py
+++ raisimGymTorch/env/envs/hound/tester.py
@@ -32,8 +32,8 @@ ob_dim = env.num_obs
 act_dim = env.num_acts
 est_dim = env.num_est
 
-# weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/hound/2023-12-02-13-02-04/full_2500.pt"
-weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/hound/2023-12-04-13-35-55/full_2500.pt"
+weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/hound/2023-12-02-13-02-04/full_3500.pt"
+# weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/hound/2023-12-04-15-40-34/full_2500.pt"
 # weight_path = args.weight
 iteration_number = weight_path.rsplit('/', 1)[1].split('_', 1)[1].rsplit('.', 1)[0]
 weight_dir = weight_path.rsplit('/', 1)[0] + '/'
@@ -71,7 +71,7 @@ else:
 
     for step in range(max_steps):
         if step % 400 == 0:
-            env.set_command(np.random.uniform(0.3, 0.3, 1),
+            env.set_command(np.random.uniform(0.6, 0.6, 1),
                             np.random.uniform(0.0, 0.0, 1),
                             np.random.uniform(0.0, 0.0, 1))
 
