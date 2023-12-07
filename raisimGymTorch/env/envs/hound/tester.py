839cb13c80ca1bb1d5aa86e9f316dd06567100ff
GijeongKim <kijoung0963@kaist.ac.kr>
2023-12-07 23:03:18 +0900

    36.4 re

--- raisimGymTorch/env/envs/hound/tester.py
+++ raisimGymTorch/env/envs/hound/tester.py
@@ -40,7 +40,7 @@ est_dim = env.num_est
 
 # weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/hound/2023-12-02-13-02-04/full_1000.pt"
 # weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/hound/2023-12-05-23-01-33/full_1500.pt"
-weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/hound/2023-12-07-22-31-12/full_500.pt"
+weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/hound/2023-12-07-22-47-08/full_500.pt"
 # weight_path = args.weight
 iteration_number = weight_path.rsplit('/', 1)[1].split('_', 1)[1].rsplit('.', 1)[0]
 weight_dir = weight_path.rsplit('/', 1)[0] + '/'
@@ -94,7 +94,7 @@ else:
                 env.set_command(command_x,command_y,command_yaw)
         else:
             if step % 400 == 0:
-                env.set_command(np.random.uniform(0.3, 0.3, 1),
+                env.set_command(np.random.uniform(0.0, 0.0, 1),
                                 np.random.uniform(0.0, 0.0, 1),
                                 np.random.uniform(0.0, 0.0, 1))
 
