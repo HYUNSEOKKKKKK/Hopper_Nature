37e907c1b616cf982d8681f1aad19b8de8ff229d
GijeongKim <kijoung0963@kaist.ac.kr>
2023-12-07 22:13:14 +0900

    36.4 re

--- raisimGymTorch/env/envs/hound/Environment.hpp
+++ raisimGymTorch/env/envs/hound/Environment.hpp
@@ -299,7 +299,7 @@ class ENVIRONMENT : public RaisimGymEnv {
 
       /// neg reward
       Eigen::VectorXd jointPosTemp(12), jointPosWeight(12);
-      jointPosWeight << 1.0, 0.7,0.7,1.,0.7,0.7,1.,0.7,0.7,1.,0.7,0.7;
+      jointPosWeight << 1.0, 0.2,0.2,1.,0.2,0.2,1.,0.2,0.2,1.,0.2,0.2;
       jointPosTemp = gc_.tail(12) - gcInit_.tail(12);
       jointPosTemp = jointPosWeight.cwiseProduct(jointPosTemp.eval());
 
