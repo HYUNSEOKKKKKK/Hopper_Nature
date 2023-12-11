1461755fafb0d1498ecca9ae4e38ede1ce734778
GijeongKim <kijoung0963@kaist.ac.kr>
2023-12-11 12:40:08 +0900

    38.67

--- raisimGymTorch/env/envs/hound/Environment.hpp
+++ raisimGymTorch/env/envs/hound/Environment.hpp
@@ -317,7 +317,7 @@ class ENVIRONMENT : public RaisimGymEnv {
 
       float posReward, negReward;
       posReward = (float)(rewards_.getReward("comAngularVel") + rewards_.getReward("comLinearVel"));
-      negReward = (float)(rewards_.getReward("jointPos") + rewards_.getReward("jointAcc") + rewards_.getReward("torque") + rewards_.getReward("footSlip") + rewards_.getReward("bodyOri") + rewards_.getReward("smoothness1") + rewards_.getReward("smoothness2"));
+      negReward = (float)(rewards_.getReward("pTarget") +rewards_.getReward("jointPos") + rewards_.getReward("jointAcc") + rewards_.getReward("torque") + rewards_.getReward("footSlip") + rewards_.getReward("bodyOri") + rewards_.getReward("smoothness1") + rewards_.getReward("smoothness2"));
       rewards_.record("negReward2", negReward); /// only for recording
 
       return (float)(std::exp(0.2 * negReward) * posReward);
