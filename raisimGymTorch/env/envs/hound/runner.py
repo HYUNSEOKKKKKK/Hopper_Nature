074a1d9aaaeb40e481e9cfea06049c501954ab84
GijeongKim <kijoung0963@kaist.ac.kr>
2023-12-11 11:54:02 +0900

    38.66

--- raisimGymTorch/env/envs/hound/runner.py
+++ raisimGymTorch/env/envs/hound/runner.py
@@ -57,7 +57,7 @@ avg_rewards = []
 actor = ppo_module.Actor(ppo_module.MLP(cfg['architecture']['policy_net'], nn.LeakyReLU, ob_dim+est_dim   , act_dim),
                          ppo_module.MultivariateGaussianDiagonalCovariance(act_dim,
                                                                            env.num_envs,
-                                                                           1.0,
+                                                                           1.5,
                                                                            NormalSampler(act_dim),
                                                                            cfg['seed']),
                          device)
@@ -172,7 +172,7 @@ for update in range(12001):
     # else:
     actor.distribution.enforce_minimum_std((torch.ones(12) * 0.3).to(device))
 
-    actor.distribution.enforce_maximum_std((torch.ones(12)*2.0).to(device))
+    actor.distribution.enforce_maximum_std((torch.ones(12)*1.5).to(device))
 
     # curriculum update. Implement it in Environment.hpp
     env.curriculum_callback()
