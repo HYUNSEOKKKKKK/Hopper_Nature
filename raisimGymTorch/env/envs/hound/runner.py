7fd708cb0161fde5a98f1d141bb4a6457b47980c
GijeongKim <kijoung0963@kaist.ac.kr>
2023-12-13 14:26:02 +0900

    42th

--- raisimGymTorch/env/envs/hound/runner.py
+++ raisimGymTorch/env/envs/hound/runner.py
@@ -168,12 +168,13 @@ for update in range(12001):
 
     actor.update()
     if update<3000:
-        actor.distribution.enforce_minimum_std((torch.ones(12) * 0.7).to(device))
-    elif update<6000:
-        actor.distribution.enforce_minimum_std((torch.ones(12) * 0.5).to(device))
+        lower_limit = 0.7
+    elif update<7000:
+        lower_limit = 0.7 - (update - 3000)/4000 * 0.4
     else:
-        actor.distribution.enforce_minimum_std((torch.ones(12) * 0.3).to(device))
+        lower_limit = 0.3
 
+    actor.distribution.enforce_minimum_std((torch.ones(12) * lower_limit).to(device))
     actor.distribution.enforce_maximum_std((torch.ones(12)*1.5).to(device))
 
     # curriculum update. Implement it in Environment.hpp
