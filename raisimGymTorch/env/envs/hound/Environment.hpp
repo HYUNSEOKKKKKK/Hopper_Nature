813551f5a6826763a3e1a42400a40e99fe11e116
GijeongKim <kijoung0963@kaist.ac.kr>
2023-12-17 01:57:12 +0900

    53th

--- raisimGymTorch/env/envs/hound/Environment.hpp
+++ raisimGymTorch/env/envs/hound/Environment.hpp
@@ -654,6 +654,7 @@ class ENVIRONMENT : public RaisimGymEnv {
       curriculum_ = (curriculum_ > 3.0) ? 3.0 : curriculum_;
 
       world_->removeObject(heightMap_);
+      heightMap_ = HeightMapSample(world_.get(),iter_%4,curriculum_,gen_,uniDist_);
 //      if (uniDist_(gen_) > 0.8){  // 10 %
 //          heightMap_ = HeightMapSample(world_.get(),0,0.0,gen_,uniDist_); /// plain
 //      }else{  // 90 %
