6b7955b9866188f0932b9a58e0710579675a310c
GijeongKim <kijoung0963@kaist.ac.kr>
2023-12-27 18:48:16 +0900

    58th_re3

--- raisimGymTorch/env/envs/hound/runner.py
+++ raisimGymTorch/env/envs/hound/runner.py
@@ -168,9 +168,9 @@ for update in range(16002):
 
     actor.update()
     if update<3000:
-        actor.distribution.enforce_minimum_std((torch.ones(12) * 0.7).to(device))
+        actor.distribution.enforce_minimum_std((torch.ones(12) * 0.9).to(device))
     elif update<6000:
-        actor.distribution.enforce_minimum_std((torch.ones(12) * 0.4).to(device))
+        actor.distribution.enforce_minimum_std((torch.ones(12) * 0.5).to(device))
     else:
         actor.distribution.enforce_minimum_std((torch.ones(12) * 0.1).to(device))
 
