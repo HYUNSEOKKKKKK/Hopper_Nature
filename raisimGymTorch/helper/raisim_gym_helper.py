f90bd9c6dd2677eddeba11f00e9943697ebf6fdd
GijeongKim <kijoung0963@kaist.ac.kr>
2023-12-28 22:33:52 +0900

    58th_re5.6

--- raisimGymTorch/helper/raisim_gym_helper.py
+++ raisimGymTorch/helper/raisim_gym_helper.py
@@ -26,7 +26,8 @@ def tensorboard_launcher(directory_path):
     # learning visualizer
     tb = program.TensorBoard()
     # tb.configure(argv=[None, '--logdir', directory_path])
-    tb.configure(argv=[None, '--logdir', directory_path,'--port',str(6008)])
+    # tb.configure(argv=[None, '--logdir', directory_path,'--port',str(6008)])
+    tb.configure(argv=[None, '--logdir', directory_path,'--port',str(6010)])
     url = tb.launch()
     print("[RAISIM_GYM] Tensorboard session created: "+url)
     webbrowser.open_new(url)
