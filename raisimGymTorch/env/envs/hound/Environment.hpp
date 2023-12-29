f5c0521c3d32a775bdcaeffa2cb3a3abbc76f04d
GijeongKim <kijoung0963@kaist.ac.kr>
2023-12-29 16:09:59 +0900

    58th_re6.1

--- raisimGymTorch/env/envs/hound/Environment.hpp
+++ raisimGymTorch/env/envs/hound/Environment.hpp
@@ -99,7 +99,7 @@ class ENVIRONMENT : public RaisimGymEnv {
     limitTargetVel_ << -0.2,0.2;
     limitFootContact_ << -0.6,2;
 //    limitFootClearance_ << -0.12,1.0; // 어차피 desired_foot_clearance 를
-            limitFootClearance_ << -0.08,1.0; // 어차피 desired_foot_clearance 를
+            limitFootClearance_ << -0.10,1.0; // 어차피 desired_foot_clearance 를
 
     /// initialize
     command_.setZero();
@@ -432,9 +432,7 @@ class ENVIRONMENT : public RaisimGymEnv {
 //      std::cout << "---------------" << std::endl;
       /// Log Barrier - limit_foot_clearance
       for (int i=0;i<4;i++){
-//          relaxedLogBarrier(0.01,limitFootClearance_(0),limitFootClearance_(1),footClearance_(i),tempReward);
-//          relaxedLogBarrier(0.03,limitFootClearance_(0),limitFootClearance_(1),footClearance_(i),tempReward);
-          relaxedLogBarrier(0.02,limitFootClearance_(0),limitFootClearance_(1),footClearance_(i),tempReward);
+          relaxedLogBarrier(0.015,limitFootClearance_(0),limitFootClearance_(1),footClearance_(i),tempReward);
           barrierFootClearance += tempReward;
 //                std::cout << i<<" th foot : " << tempReward << std::endl;
       }
@@ -454,7 +452,7 @@ class ENVIRONMENT : public RaisimGymEnv {
       rewards_.record("barrierTargetVel", barrierTargetVel);
       rewards_.record("barrierFootContact", barrierFootContact);
       rewards_.record("barrierFootClearance", barrierFootClearance);
-//        if (barrierFootClearance < -40) {
+//        if (barrierFootClearance < -100) {
 //            std::cout << "barrierJointPos : " <<  barrierJointPos << std::endl;
 //            std::cout << "barrierBodyHeight : " <<  barrierBodyHeight << std::endl;
 //            std::cout << "barrierBaseMotion : " <<  barrierBaseMotion << std::endl;
--- raisimGymTorch/env/envs/hound/tester.py
+++ raisimGymTorch/env/envs/hound/tester.py
@@ -38,8 +38,8 @@ ob_dim = env.num_obs
 act_dim = env.num_acts
 est_dim = env.num_est
 
-weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/hound/2023-12-28-20-14-01/full_2000.pt"
-# weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/hound/2023-12-08-11-12-57_hiking/full_2000.pt"
+weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/hound/2023-12-28-22-35-14/full_8000.pt"
+# weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/hound/2023-12-08-11-12-57_hiking/full_8000.pt"
 # weight_path = args.weight
 iteration_number = weight_path.rsplit('/', 1)[1].split('_', 1)[1].rsplit('.', 1)[0]
 weight_dir = weight_path.rsplit('/', 1)[0] + '/'
@@ -51,7 +51,7 @@ else:
     print("Loaded weight from {}\n".format(weight_path))
     start = time.time()
 
-    env.set_terrain(3,2.5,1.0)
+    env.set_terrain(3,3.0,1.0)
     env.set_initial(0)
 
     env.reset()
