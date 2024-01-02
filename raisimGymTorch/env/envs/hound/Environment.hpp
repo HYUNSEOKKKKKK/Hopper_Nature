d37af27608cc84418d76e5f64d797eb74683a64c
GijeongKim <kijoung0963@kaist.ac.kr>
2024-01-02 17:14:05 +0900

    59th_3

--- raisimGymTorch/env/envs/hound/Environment.hpp
+++ raisimGymTorch/env/envs/hound/Environment.hpp
@@ -91,8 +91,7 @@ class ENVIRONMENT : public RaisimGymEnv {
         limitJointPos_.row(i*3+0) << -0.523599,0.523599; // roll : (-pi/6, pi/6)
         limitJointPos_.row(i*3+1) << hip-0.785398,hip+0.785398; // hip
 //                limitJointPos_.row(i*3+2) << -2.6,-0.52; // knee
-//                limitJointPos_.row(i*3+2) << -2.4,-0.52; // knee
-                limitJointPos_.row(i*3+2) << -2.2,-0.52; // knee
+                limitJointPos_.row(i*3+2) << -2.3,-0.52; // knee
 //        limitJointPos_.row(i*3+2) << -2.0,-0.52; // knee
     }
     limitBodyHeight_ << 0.54, 0.72;
