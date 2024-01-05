9a67580c9d7450d08db53d6a722d92558f0d048c
GijeongKim <kijoung0963@kaist.ac.kr>
2024-01-05 17:32:55 +0900

    71th

--- raisimGymTorch/env/envs/hound/tester.py
+++ raisimGymTorch/env/envs/hound/tester.py
@@ -51,7 +51,7 @@ else:
     print("Loaded weight from {}\n".format(weight_path))
     start = time.time()
 
-    env.set_terrain(0,0.0,1.0)
+    env.set_terrain(2,3.0,1.0)
     env.set_initial(0)
 
     env.reset()
