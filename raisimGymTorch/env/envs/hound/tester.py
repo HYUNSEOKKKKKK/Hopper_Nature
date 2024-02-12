d439084ae0fb18e8b73c3daf2c12c91f62b87c04
GijeongKim <kijoung0963@kaist.ac.kr>
2024-02-12 17:22:16 +0900

    digit 1st_revised8 (good)

--- raisimGymTorch/env/envs/hound/tester.py
+++ raisimGymTorch/env/envs/hound/tester.py
@@ -38,7 +38,7 @@ ob_dim = env.num_obs
 act_dim = env.num_acts
 est_dim = env.num_est
 
-weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/digit/2024-02-07-11-28-07/full_5500.pt"
+weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/digit/2024-02-07-11-28-07/full_8000.pt"
 # weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/hound/2023-12-08-11-12-57_hiking/full_10000.pt"
 # weight_path = args.weight
 iteration_number = weight_path.rsplit('/', 1)[1].split('_', 1)[1].rsplit('.', 1)[0]
@@ -51,7 +51,7 @@ else:
     print("Loaded weight from {}\n".format(weight_path))
     start = time.time()
 
-    env.set_terrain(0,3.0,1.0)
+    env.set_terrain(0,0.0,1.0)
     env.set_initial(0)
 
     env.reset()
