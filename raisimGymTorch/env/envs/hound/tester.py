4.5 th -> fix the error about joint acc computation & lower torque reward as before

raisimGymTorch/env/envs/hound/tester.py
@@ -39,7 +39,7 @@ act_dim = env.num_acts
 est_dim = env.num_est
 
 # weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/dhal_one_leg/2.1th_closed_loop_highPD_2025-02-20-00-42-22/full_2000.pt"
-weight_path = ("/media/gijeong/T7/raisimGymTorch/data/dhal_one_leg/2025-02-23-14-12-05/full_6000.pt")
+weight_path = ("/media/gijeong/T7/raisimGymTorch/data/dhal_one_leg/2025-02-23-14-12-05/full_10000.pt")
 iteration_number = weight_path.rsplit('/', 1)[1].split('_', 1)[1].rsplit('.', 1)[0]
 weight_dir = weight_path.rsplit('/', 1)[0] + '/'
 
@@ -92,7 +92,7 @@ else:
                                     np.random.uniform(-0., 0., 1),
                                     np.random.uniform(-0., 0., 1))
 
-        time.sleep(0.012)
+        time.sleep(0.006)
         with torch.no_grad():
             obs = env.observe(False)
             est_out = loaded_graph_est.architecture(torch.from_numpy(obs).cpu())