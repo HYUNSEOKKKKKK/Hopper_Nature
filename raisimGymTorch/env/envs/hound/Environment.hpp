2442cb8a1f593a2b4e37ad2ce77422460c13b095
GijeongKim <kijoung0963@kaist.ac.kr>
2025-02-27 13:19:00 +0900

    4.6 th

--- raisimGymTorch/env/envs/hound/Environment.hpp
+++ raisimGymTorch/env/envs/hound/Environment.hpp
@@ -584,12 +584,12 @@ class ENVIRONMENT : public RaisimGymEnv {
       barrierTargetVel += tempReward;
       /// Log Barrier - limit_foot_contact
       for (int i=0;i<numLegs_;i++){
-          relaxedLogBarrier(0.08,limitFootContact_(0),limitFootContact_(1),footContactDouble_(i),tempReward);
+          relaxedLogBarrier(0.07,limitFootContact_(0),limitFootContact_(1),footContactDouble_(i),tempReward);
           barrierFootContact += tempReward;
       }
       /// Log Barrier - limit_foot_clearance
       for (int i=0;i<numLegs_;i++){
-          relaxedLogBarrier(0.020,limitFootClearance_(0),limitFootClearance_(1),footClearance_(i),tempReward);
+          relaxedLogBarrier(0.018,limitFootClearance_(0),limitFootClearance_(1),footClearance_(i),tempReward);
           barrierFootClearance += tempReward;
       }
       /// Log Barrier - limit_body_contact
