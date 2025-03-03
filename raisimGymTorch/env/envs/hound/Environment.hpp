d295bc8235a7cc7e0a0ec4344ccb61b6dd9d1732
GijeongKim <kijoung0963@kaist.ac.kr>
2025-03-03 16:27:14 +0900

    5.1 th

--- raisimGymTorch/env/envs/hound/Environment.hpp
+++ raisimGymTorch/env/envs/hound/Environment.hpp
@@ -119,7 +119,7 @@ class ENVIRONMENT : public RaisimGymEnv {
     limitBaseMotion_.row(1) << -0.8,0.8; // roll
     limitJointVel_ << -6,6; // max vel limit is 9
     limitTargetVel_ << -0.6,0.6;
-    limitFootContact_ << -0.3,2;
+    limitFootContact_ << -0.6,2;
     limitFootClearance_ << -0.08,1.0; // 어차피 desired_foot_clearance 를
     limitBodyContact_ << -1.0,1.0;
 
@@ -528,7 +528,7 @@ class ENVIRONMENT : public RaisimGymEnv {
       if (!standingMode_){ /// walking
           /// footContactDouble_ -> limit_foot_contact 에 있도록 (-0.3,3) -> Gait Enforcing (요 -0.3 이 벗어나도 되는 범위)
           for(int i=0; i<numLegs_; i++) {
-              if (footContact_ > 1) { footContactDouble_(i) = 1.0 * footContactPhase_(i); }
+              if (footContact_ > 0) { footContactDouble_(i) = 1.0 * footContactPhase_(i); }
               else { footContactDouble_(i) = -1.0 * footContactPhase_(i); }
           }
           /// footClearance_ -> limit_foot_clearance 에 있도록 (-0.12,0.12) -> foot 드는 거 enforcing
