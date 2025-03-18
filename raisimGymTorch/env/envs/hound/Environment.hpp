8304191cdd9383a183425e6d4af64187c52bf413
GijeongKim <kijoung0963@kaist.ac.kr>
2025-03-18 15:51:22 +0900

    v2.32temp, enlarged standingMode

--- raisimGymTorch/env/envs/hound/Environment.hpp
+++ raisimGymTorch/env/envs/hound/Environment.hpp
@@ -225,7 +225,8 @@ class ENVIRONMENT : public RaisimGymEnv {
 //            double initializeCurriculum = 1.0; /// no curriculum
 
     /// with standing mode
-    if (uniDist_(gen_) > 0.8) { // 10 %
+//    if (uniDist_(gen_) > 0.8) { // 10 %
+    if (uniDist_(gen_) > 0.6) { // 20 %
         standingMode_ = true;
         command_.setZero();
     }else{
