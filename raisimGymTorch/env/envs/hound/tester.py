3508caea67056a2cffe2ce3e4daff79c0582cbca
GijeongKim <kijoung0963@kaist.ac.kr>
2025-03-21 01:32:37 +0900

    v2.32temp5.1 _ scale correction

--- raisimGymTorch/env/envs/hound/tester.py
+++ raisimGymTorch/env/envs/hound/tester.py
@@ -39,7 +39,7 @@ act_dim = env.num_acts
 est_dim = env.num_est
 
 # weight_path = "/home/gijeong/workspace/raisimLib/hound/raisimGymTorch/data/dhal_one_leg/v2.32temp4.2_2025-03-20-02-21-18/full_4000.pt"
-weight_path = ("/media/gijeong/T7/raisimGymTorch/data/dhal_one_leg/2025-03-20-16-12-18/full_500.pt")
+weight_path = ("/media/gijeong/T7/raisimGymTorch/data/dhal_one_leg/2025-03-20-19-34-14/full_4000.pt")
 iteration_number = weight_path.rsplit('/', 1)[1].split('_', 1)[1].rsplit('.', 1)[0]
 weight_dir = weight_path.rsplit('/', 1)[0] + '/'
 
@@ -50,7 +50,7 @@ else:
     print("Loaded weight from {}\n".format(weight_path))
     start = time.time()
 
-    env.set_terrain(0,0.0,0.7)
+    env.set_terrain(1,0.2,0.7)
     env.set_initial(0)
 
     env.reset()
@@ -94,7 +94,7 @@ else:
 
         if step % 400 == 0:
             env.reset()
-            env.set_command(np.random.uniform(0.6, 0.6, 1),0.0,0.0)
+            env.set_command(np.random.uniform(0.0, 0.0, 1),0.0,0.6)
         elif (step-200)%400 == 0:
             env.set_command(np.random.uniform(0.0, 0.0, 1),0.0,0.0)
         time.sleep(0.010)
